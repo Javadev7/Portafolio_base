6891bffa08217b8bed38d0ebfbfe024332a1b15a
Javadeveloper7 <95065154+Javadeveloper7@users.noreply.github.com>
2022-07-03 12:44:57 -0500

    Wrap

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,17 +1,28 @@
 import React, {useState, useEffect} from 'react';
 import {motion} from 'framer-motion';
 
-import { images} from '../../constants'
+import { AppWrap } from '../../wrapper';
 import './About.scss';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  {title: 'Web Development', description: ' I am a good web development', imgUrl: images.about01 },
-  {title: 'Frotend', description: ' I am a good Frontend', imgUrl: images.about02},
-  {title: 'Backend', description: ' I am a good Backend', imgUrl: images.about03},
-  {title: 'FullStack developer', description: ' I am a good Fullstack developer', imgUrl: images.about04} 
-]
 
+/**
+ * I'm using the Sanity.io API to fetch data from a database and then I'm using the data to populate a
+ * React component.
+ * I'm using
+ * @returns An array of objects.
+ */
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+
+  /* It's returning a React component. */
   return (
     <>
       <h2 className='head-text'>I know that <span>Good Development</span> means <span>Good Business</span>
@@ -26,7 +37,7 @@ const About = () => {
             className='app__profiles-item'
             key={about.title + index}
             >
-              <img src={about.imgUrl} alt={about.title} />
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
               <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
         </motion.div>
@@ -36,4 +47,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about');
